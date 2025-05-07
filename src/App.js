8e9e60c5bbc9d74d8b91e14cf1596b53b28c32f4
Aryo Prakarsa <30886148+aryoprakarsa@users.noreply.github.com>
2025-05-07 18:25:01 +0700

    update index styling shadow

--- src/App.js
+++ src/App.js
@@ -5,13 +5,8 @@ import logo from "./assets/d5ab8991-1bb7-4ae2-bfa8-11de93a58f74.png";
 function App() {
   return (
     <div className="text-center text-light d-flex flex-column align-items-center justify-content-center min-vh-100 pt-5 pb-5">
-      <div className="cat-wrapper mb-3">
-        <img
-          src={logo}
-          alt="Noxturna Logo"
-          width="240"
-          style={{ filter: "drop-shadow(0 8px 12px rgba(0, 0, 0, 0.5))" }}
-        />
+      <div className="cat-wrapper mb-1">
+        <img src={logo} alt="Noxturna Logo" width="240" />
       </div>
       <h1>Noxturna</h1>
       <p>カミングスーン</p>
