281dd5decf4be2400275a47dfb7e92813e1a1200
Aryo Prakarsa <30886148+aryoprakarsa@users.noreply.github.com>
2025-05-07 22:39:57 +0700

    fix content position

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import logo from "./assets/d5ab8991-1bb7-4ae2-bfa8-11de93a58f74.png";
 
 function App() {
   return (
-    <div className="App">
-      <div className="cat-wrapper">
-        <img src={logo} alt="Noxturna Logo" />
+    <div className="container-fluid min-vh-100 d-flex flex-column justify-content-center align-items-center text-center">
+      <div className="cat-wrapper mb-1">
+        <img src={logo} alt="Noxturna Logo" width="240" />
       </div>
       <h1>Noxturna</h1>
       <p>カミングスーン</p>
