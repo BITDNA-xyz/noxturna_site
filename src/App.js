6d9af442ac21ad15e5c5e5e63185355745a6e1d9
Aryo Prakarsa <30886148+aryoprakarsa@users.noreply.github.com>
2025-05-09 15:23:22 +0700

    fix style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from "./assets/d5ab8991-1bb7-4ae2-bfa8-11de93a58f74.png";
 
 function App() {
   return (
-    <div className="container-fluid min-vh-100 d-flex flex-column justify-content-center align-items-center text-center">
+    <div className="page-center App">
       <div className="cat-wrapper mb-1">
         <img src={logo} alt="Noxturna Logo" width="240" />
       </div>
